d53abf43daf4db80f5bbd93458184dbeb441d79c
bopzen <boppp@abv.bg>
2024-04-10 10:29:23 +0300

    Added Edit and Delete views

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -26,6 +26,12 @@ namespace ShoppintList.Controllers
             return View(products);
         }
 
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Add(ProductFormModel model)
         {
@@ -39,5 +45,37 @@ namespace ShoppintList.Controllers
 
             return RedirectToAction("All");
         }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _data.Products.Find(id);
+
+            return View(new ProductFormModel()
+            {
+                Name = product.Name
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Product model) 
+        {
+            var product = _data.Products.Find(id);
+            product.Name = model.Name;
+
+            _data.SaveChanges();
+
+            return RedirectToAction("All");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _data.Products.Find(id);
+
+			_data.Products.Remove(product);
+			_data.SaveChanges();
+
+			return RedirectToAction("All");
+		}
+
     }
 }
